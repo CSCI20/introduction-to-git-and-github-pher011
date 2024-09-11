acd6eb23124f27c64cb5656edd5465eb6b37ef91
pher011 <pher011@student.butte.edu>
2024-09-10 19:51:47 -0700

    Add files via upload

---
+++ OutputandComments.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {;
+  cout<<"   A________A                |\n";
+  cout<<"__|  .   .   |__        __(\n";
+  cout<<"  |__________|__________) \n";
+  cout<<"             |          | \n";
+  cout<<"             |__________| \n";
+  cout<<"             |          |\n";
+}
